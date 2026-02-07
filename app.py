b88024a88b6935f4e077c3679dd1fdd07acbd33b
jayprakashupadhyay573 <upadhyayj573@gmail.com>
2026-02-07 22:35:04 +0530

    update app for cloud deployment

--- app.py
+++ app.py
@@ -62,5 +62,8 @@ def predict():
             suggestion="Please enter values within allowed range."
         )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
